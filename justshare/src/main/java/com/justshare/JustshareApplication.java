bedaf812d71f41db8cffbab0cb7e39fafc4de713
Abhinav Gupta <abhigupta.195719@gmail.com>
2026-08-12 14:33:45 +0530

    small bug fix

--- justshare/src/main/java/com/justshare/JustshareApplication.java
+++ justshare/src/main/java/com/justshare/JustshareApplication.java
@@ -9,9 +9,7 @@ public class JustshareApplication {
 
 	public static void main(String[] args) {
 
-//		SpringApplication.run(JustshareApplication.class, args);
-		System.out.println("DB_USERNAME = " + System.getenv("DB_USERNAME"));
-		System.out.println("TEST_ENV = " + System.getenv("TEST_ENV"));
+		SpringApplication.run(JustshareApplication.class, args);
 
 	}
 
