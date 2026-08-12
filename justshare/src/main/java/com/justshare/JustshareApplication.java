6fa7209ce805286d0759be25b7326a750faaea9a
Abhinav Gupta <abhigupta.195719@gmail.com>
2026-08-12 14:06:00 +0530

    Initial JustShare deployment

--- justshare/src/main/java/com/justshare/JustshareApplication.java
+++ justshare/src/main/java/com/justshare/JustshareApplication.java
@@ -9,7 +9,9 @@ public class JustshareApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(JustshareApplication.class, args);
+//		SpringApplication.run(JustshareApplication.class, args);
+		System.out.println("DB_USERNAME = " + System.getenv("DB_USERNAME"));
+		System.out.println("TEST_ENV = " + System.getenv("TEST_ENV"));
 
 	}
 
